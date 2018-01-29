debce023c74919f178c97c0eb04018d717c22eb8
installgen2 <gen2@gen2.space>
2018-01-29 09:59:07 -0700

    Add default sort direction, class and all methods

--- src/query.js
+++ src/query.js
@@ -1,23 +1,38 @@
-"use strict"
+"use strict";
 
+/**
+ * Query builder class
+ */
 class DbQuery {
+  /**
+   * Construct query builder class
+   */
   constructor (Model, db) {
     this._Model = Model;
     this._db = db;
     this.pts = [];
   }
 
+  /**
+   * Set maximum returned Model instances
+   */
   limit (amt) {
     this.pts.push ({ type: "limit", limitAmount: amt });
     return this;
   }
 
+  /**
+   * Skip the first specified amount of returned Model instances
+   */
   skip (amt) {
     this.pts.push ({ type: "skip", skipAmount: amt });
     return this;
   }
 
-  sort (key, directionStr) {
+  /**
+   * Sort returned Model instances by a key and optional direction (default descending)
+   */
+  sort (key, directionStr = "desc") {
     directionStr = directionStr.toString ();
 
     let desc = null;
@@ -34,6 +49,9 @@ class DbQuery {
     return this;
   }
 
+  /**
+   * Filter only Model instances where the specified key matches the specified val, can also be given a filter object
+   */
   where (key, value = null) {
     if (key instanceof Object && value == null) {
       const filter = key;
@@ -45,41 +63,66 @@ class DbQuery {
     }
   }
 
+  /**
+   * Only return Model instances where the value of the specified key is greater than the specified amount
+   */
   gt (key, min) {
     this.pts.push ({ type: "gt", min: min, key: key });
     return this;
   }
 
+  /**
+   * Only return model instances where the value of the specified key is less than the specified amount
+   */
   lt (key, max) {
     this.pts.push ({ type: "lt", max: max, key: key });
     return this;
   }
 
+  /**
+   * Only return Model instances where the value of the specified key is greater or equal to than the specified amount
+   */
   gte (key, min) {
     this.pts.push ({ type: "gte", min: min, key: key });
     return this;
   }
 
+  /**
+   * Only return model instances where the value of the specified key is less than or equal to the specified amount
+   */
   lte (key, min) {
     this.pts.push ({ type: "lte", max: max, key: key });
     return this;
   }
 
+  /**
+   * Finalize this query and return all matching Model instances
+   */
   async find () {
     return await this._db.findModels (this._Model, this);
   }
 
+  /**
+   * Finalize this query and return one matching Model instance
+   */
   async findOne () {
     return await this._db.findModel (this._Model, this);
   }
 
+  /**
+   * Finalize this query and return the total amount of matching Model instances
+   */
   async count () {
     return await this._db.countModels (this._Model, this);
   }
 
+  /**
+   * Finalize this query and remove all matching Model instances
+   */
   async remove () {
     await this._db.removeModels (this._Model, this);
   }
 }
 
+// Exports
 module.exports = exports = DbQuery;
