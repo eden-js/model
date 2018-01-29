136b0a711968904d0c644d269738c6a230cdf591
installgen2 <gen2@gen2.space>
2018-01-29 07:28:15 -0700

    Reformat to standard intendation style

--- src/index.js
+++ src/index.js
@@ -9,105 +9,105 @@ const MongoPlug   = require ('./plugs/mongo');
 const RethinkPlug = require ('./plugs/rethink');
 
 function modelCollectionId (Model) {
-	return pluralize (Model.name.toLowerCase ());
+  return pluralize (Model.name.toLowerCase ());
 }
 
 function modelInstanceCollectionId (model) {
-	return pluralize (model.constructor.name.toLowerCase ());
+  return pluralize (model.constructor.name.toLowerCase ());
 }
 
 class DbApi {
-	constructor (dbPlug) {
-		this._plug = dbPlug;
+  constructor (dbPlug) {
+    this._plug = dbPlug;
 
-		this.saveModel       = this.saveModel.bind (this);
-		this.findModelById   = this.findModelById.bind (this);
-		this.findModels      = this.findModels.bind (this);
-		this.findModel       = this.findModel.bind (this);
-		this.countModels     = this.countModels.bind (this);
-		this.removeModelById = this.removeModelById.bind (this);
-		this.removeModels    = this.removeModels.bind (this);
-	}
+    this.saveModel       = this.saveModel.bind (this);
+    this.findModelById   = this.findModelById.bind (this);
+    this.findModels      = this.findModels.bind (this);
+    this.findModel       = this.findModel.bind (this);
+    this.countModels     = this.countModels.bind (this);
+    this.removeModelById = this.removeModelById.bind (this);
+    this.removeModels    = this.removeModels.bind (this);
+  }
 
-	async saveModel (model) {
-		const collectionId = modelInstanceCollectionId (model);
+  async saveModel (model) {
+  const collectionId = modelInstanceCollectionId (model);
 
-		if (model.id == null) {
-			model.id = await this._plug.insert (collectionId, model.get ());
-		} else {
-			await this._plug.replaceById (collectionId, model.id, model.get ());
-		}
-	}
+    if (model.id == null) {
+      model.id = await this._plug.insert (collectionId, model.get ());
+    } else {
+      await this._plug.replaceById (collectionId, model.id, model.get ());
+    }
+  }
 
-	async findModelById (Model, id) {
-		const collectionId = modelCollectionId (Model);
-		const foundValue = await this._plug.findById (collectionId, id);
+  async findModelById (Model, id) {
+    const collectionId = modelCollectionId (Model);
+    const foundValue = await this._plug.findById (collectionId, id);
 
-		if (foundValue == null) {
-			return null;
-		}
+    if (foundValue == null) {
+      return null;
+    }
 
-		return new Model (foundValue, id);
-	}
+    return new Model (foundValue, id);
+  }
 
-	async findModels (Model, query) {
-		const collectionId = modelCollectionId (Model);
-		const foundValues = await this._plug.find (collectionId, query);
+  async findModels (Model, query) {
+    const collectionId = modelCollectionId (Model);
+    const foundValues = await this._plug.find (collectionId, query);
 
-		const models = [];
+    const models = [];
 
-		for (const foundValue of foundValues) {
-			models.push (new Model (foundValue.object, foundValue.id));
-		}
+    for (const foundValue of foundValues) {
+      models.push (new Model (foundValue.object, foundValue.id));
+    }
 
-		return models;
-	}
+    return models;
+  }
 
-	async findModel (Model, query) {;
-		const collectionId = modelCollectionId (Model)
-		const foundValue = await this._plug.findOne (collectionId, query);
+  async findModel (Model, query) {;
+    const collectionId = modelCollectionId (Model)
+    const foundValue = await this._plug.findOne (collectionId, query);
 
-		if (foundValue == null) {
-			return null;
-		}
+    if (foundValue == null) {
+      return null;
+    }
 
-		return new Model (foundValue.object, foundValue.id);
-	}
+    return new Model (foundValue.object, foundValue.id);
+  }
 
-	async countModels (Model, query) {
-		const collectionId = modelCollectionId (Model);
-		return await this._plug.count (collectionId, query);
-	}
+  async countModels (Model, query) {
+    const collectionId = modelCollectionId (Model);
+    return await this._plug.count (collectionId, query);
+  }
 
-	async removeModelById (Model, id) {
-		const collectionId = modelCollectionId (Model);
-		await this._plug.removeById (collectionId, id);
-	}
+  async removeModelById (Model, id) {
+    const collectionId = modelCollectionId (Model);
+    await this._plug.removeById (collectionId, id);
+  }
 
-	async removeModels (Model, query) {
-		const collectionId = modelCollectionId (Model);
-		await this._plug.removeById (collectionId, query);
-	}
+  async removeModels (Model, query) {
+    const collectionId = modelCollectionId (Model);
+    await this._plug.removeById (collectionId, query);
+  }
 }
 
 class Db {
-	constructor (dbPlug) {
-		this._dbApi = new DbApi (dbPlug);
+  constructor (dbPlug) {
+    this._dbApi = new DbApi (dbPlug);
 
-		this.register = this.register.bind (this);
-	}
+    this.register = this.register.bind (this);
+  }
 
-	register (Model) {
-		Model._$_db = this._dbApi;
-	}
+  register (Model) {
+    Model._$_db = this._dbApi;
+  }
 }
 
 module.exports = exports = {
-	Db      : Db,
-	DbModel : DbModel,
+  Db      : Db,
+  DbModel : DbModel,
 
-	plugs: {
-		MongoPlug   : MongoPlug,
-		RethinkPlug : RethinkPlug,
-	},
+  plugs: {
+    MongoPlug   : MongoPlug,
+    RethinkPlug : RethinkPlug,
+  },
 };
