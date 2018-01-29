017a064b0b574f710387608f0f265e696e73b69c
installgen2 <gen2@gen2.space>
2018-01-29 08:07:10 -0700

    Fully annotate methods of Model class

--- src/model.js
+++ src/model.js
@@ -6,7 +6,13 @@ const DotProp = require ('dot-prop');
 // Require local dependencies
 const DbQuery = require ('./query');
 
+/**
+ * Extendable Model class
+ */
 class DbModel {
+  /**
+   * Construct Model class
+   */
   constructor (data, id = null) {
     this._data = data;
     this._id = id;
@@ -14,6 +20,9 @@ class DbModel {
     this._data._id = this._id;
   }
 
+  /**
+   * Get internal data by dot-prop key
+   */
   get (key = '') {
     if (key.length === 0) {
       return this._data;
@@ -22,6 +31,9 @@ class DbModel {
     DotProp.get (this._data, key);
   }
 
+  /**
+   * Set internal data by dot-prop key and value, or object containing update information
+   */
   set (key, value = null) {
     if (key instanceof Object && value == null) {
       const updateObj = key;
@@ -32,11 +44,19 @@ class DbModel {
     DotProp.set (this._data, key, value);
   }
 
+  /**
+   * Save this Model instance's data to the database
+   */
   async save () {
     await this.constructor._$_db.saveModel (this);
   }
 
+  /**
+   * Remove this Model instance's data from the database
+   */
   async remove () {
+    // TODO: Should delete class itself too?
+
     if (this._id == null) {
       throw new Error ('Model not stored in database');
     }
@@ -44,22 +64,43 @@ class DbModel {
     await this.constructor._$_db.removeModelById (this.constructor, this._id);
   }
 
+  /**
+   * Create database query builder from self and associated DB API
+   */
   static query () {
     return new DbQuery (this, this._$_db);
   }
 
+  /**
+   * Query-less database actions using simple filter
+   */
+  // Find Model instances by simple filter
   static async find (filter = {}) { return await this.query ().where (filter).find () }
+  // Find single Model instance by simple filter
   static async findOne (filter = {}) { return this.query ().where (filter).findOne () }
+  // Count stored Model instances by simple filter
   static async count (filter = {}) { return this.query ().where (filter).count () }
+  // Remove stored Model instance by simple filter
   static async remove (filter = {}) { return this.query ().where (filter).remove () }
 
+  /**
+   * Query constructor methods
+   */
+  // Create a query builder with initial `limit` set
   static limit (amt) { return this.query ().limit (amt) }
+  // Create a query builder with initial `skip` set
   static skip (amt) { return this.query ().skip (amt) }
+  // Create a query builder with initial `sort` set
   static sort (key, direction) { return this.query ().sort (key, direction) }
+  // Create a query builder with initial `where` set
   static where (key, value) { return this.query ().where (key, value) }
+  // Create a query builder with initial `gt` set
   static gt (key, min) { return this.query ().gt (key, min) }
+  // Create a query builder with initial `lt` set
   static lt (key, max) { return this.query ().lt (key, max) }
+  // Create a query builder with initial `gte` set
   static gte (key, min) { return this.query ().gte (key, min) }
+  // Create a query builder with initial `gt` set
   static lte (key, max) { return this.query ().gt (key, max) }
 }
 
